4412cb4d8d93240a1ee5c5d6376f3c372ad9b248
erso <edusillva784@gmail.com>
2023-10-30 11:02:24 -0300

    Estruturação dos pacotes

--- src/cliente.py
+++ src/cliente.py
@@ -1,199 +1,216 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import List
+import sys
+import pacman
 
+NOVO = '0'
+SENHA = '1'
+ENTRA = '2'
+LIDERES='3'
+LISTA_CONECTADOS = '4'
+LISTA_TODOS = '5'
+INICIA= '6'
+DESAFIO = '7'
+DESLOGA = '8'
+MOVE = '9'
+ATRASO = '10'
+ENCERRA  ='11'
+TCHAU = '12'
+TABULEIRO = '13'
 
 class Comandos(ABC):
     @abstractmethod
-    def comandos_novo(self, dados: list) -> None:
+    def novo(self, dados: list) -> None:
         pass
 
     @abstractmethod
-    def comandos_senha(self, dados: list) -> None:
+    def senha(self, dados: list) -> None:
         pass
 
     @abstractmethod
-    def comandos_entra(self, dados: list) -> None:
+    def entra(self, dados: list) -> None:
         pass
 
     @abstractmethod
-    def comandos_lideres(self) -> None:
+    def lideres(self) -> None:
         pass
 
     @abstractmethod
-    def comandos_l(self) -> None:
+    def l(self) -> None:
         pass
 
     @abstractmethod
-    def comandos_inicia(self) -> None:
+    def inicia(self) -> None:
         pass
 
     @abstractmethod
-    def comandos_desafio(self, dados: list) -> None:
+    def desafio(self, dados: list) -> None:
         pass
 
     @abstractmethod
-    def comandos_move(self, dados: list) -> None:
+    def move(self, dados: list) -> None:
         pass
 
     @abstractmethod
-    def comandos_atraso(self) -> None:
+    def atraso(self) -> None:
         pass
 
     @abstractmethod
-    def comandos_encerra(self) -> None:
+    def encerra(self) -> None:
         pass
 
     @abstractmethod
-    def comandos_sai(self) -> None:
+    def sai(self, dados: list) -> None:
         pass
 
     @abstractmethod
-    def comandos_tchau(self) -> None:
+    def tchau(self) -> None:
         pass
-
 
 class Estados(Comandos, ABC):
 
     @property
-    def cliente(self) -> Clientes:
+    def cliente(self) -> Cliente:
         return self._cliente
 
     @cliente.setter
-    def cliente(self, cliente: Clientes) -> None:
+    def cliente(self, cliente: Cliente) -> None:
         self._cliente = cliente
 
     def executa_comando(self, dados: list):
         pass
 
-
 class EstadosNeutro(Estados):  # novo entra lideres l tchau
 
-    def comandos_novo(self, dados: list) -> None:
+    def novo(self, dados: list) -> None:
+        usuario,senha = dados
+        return f'{NOVO}{len(usuario)}{usuario}{len(senha)}{senha}'
+
+    def senha(self, dados: list) -> None:
         pass
 
-    def comandos_senha(self, dados: list) -> None:
+    def entra(self, dados: list) -> None:
+        usuario, senha = dados
+        return f'{ENTRA}{len(usuario)}{usuario}{len(senha)}{senha}'
+
+    def lideres(self) -> None:
+        return f'{LIDERES}'
+
+    def l(self) -> None:
+        return f'{LISTA_CONECTADOS}'
+
+    def inicia(self) -> None:
         pass
 
-    def comandos_entra(self, dados: list) -> None:
+    def desafio(self, dados: list) -> None:
         pass
 
-    def comandos_lideres(self) -> None:
+    def move(self, dados: list) -> None:
         pass
 
-    def comandos_l(self) -> None:
+    def atraso(self) -> None:
         pass
 
-    def comandos_inicia(self) -> None:
+    def encerra(self) -> None:
         pass
 
-    def comandos_desafio(self, dados: list) -> None:
+    def sai(self, dados: list) -> None:
         pass
 
-    def comandos_move(self, dados: list) -> None:
-        pass
-
-    def comandos_atraso(self) -> None:
-        pass
-
-    def comandos_encerra(self) -> None:
-        pass
-
-    def comandos_sai(self) -> None:
-        pass
-
-    def comandos_tchau(self) -> None:
-        pass
+    def tchau(self) -> None:
+        return f'{TCHAU}'
 
     def executa_comando(self, dados: list):
         pass
-
 
 class EstadosConectado(Estados):  # senha lideres l inicia desafio sai
 
-    def comandos_novo(self, dados: list) -> None:
+    def novo(self, dados: list) -> None:
         pass
 
-    def comandos_senha(self, dados: list) -> None:
+    def senha(self, dados: list) -> None:
+        usuario, senha_antiga, senha_nova = dados
+        return f'{SENHA}{len(usuario)}{usuario}{len(senha_antiga)}{senha_antiga}{len(senha_nova)}{senha_nova}'
+
+    def entra(self, dados: list) -> None:
         pass
 
-    def comandos_entra(self, dados: list) -> None:
+    def lideres(self) -> None:
+        return f'{LIDERES}'
+
+    def l(self) -> None:
+        return f'{LISTA_CONECTADOS}'
+
+    def inicia(self) -> None:
+        return f'{INICIA}'
+
+    def desafio(self, dados: list) -> None:
+        usuario = dados
+        return f'{DESAFIO}{len(usuario)}{usuario}'
+
+    def move(self, dados: list) -> None:
         pass
 
-    def comandos_lideres(self) -> None:
+    def atraso(self) -> None:
         pass
 
-    def comandos_l(self) -> None:
+    def encerra(self) -> None:
         pass
 
-    def comandos_inicia(self) -> None:
-        pass
+    def sai(self, dados: list) -> None:
+        usuario = dados
+        return f'{DESLOGA}{len(usuario)}{usuario}'
 
-    def comandos_desafio(self, dados: list) -> None:
-        pass
-
-    def comandos_move(self, dados: list) -> None:
-        pass
-
-    def comandos_atraso(self) -> None:
-        pass
-
-    def comandos_encerra(self) -> None:
-        pass
-
-    def comandos_sai(self) -> None:
-        pass
-
-    def comandos_tchau(self) -> None:
-        pass
-
-    def executa_comando(self, dados: list):
-        pass
-
-
-class EstadosJogando(Estados):  # lideres l move atraso encerra
-
-    def comandos_novo(self, dados: list) -> None:
-        pass
-
-    def comandos_senha(self, dados: list) -> None:
-        pass
-
-    def comandos_entra(self, dados: list) -> None:
-        pass
-
-    def comandos_lideres(self) -> None:
-        pass
-
-    def comandos_l(self) -> None:
-        pass
-
-    def comandos_inicia(self) -> None:
-        pass
-
-    def comandos_desafio(self, dados: list) -> None:
-        pass
-
-    def comandos_move(self, dados: list) -> None:
-        pass
-
-    def comandos_atraso(self) -> None:
-        pass
-
-    def comandos_encerra(self) -> None:
-        pass
-
-    def comandos_sai(self) -> None:
-        pass
-
-    def comandos_tchau(self) -> None:
+    def tchau(self) -> None:
         pass
 
     def executa_comando(self, dados: list):
         pass
 
+class EstadosJogando(Estados):  # move atraso encerra
 
-class Clientes:
+    def novo(self, dados: list) -> None:
+        pass
+
+    def senha(self, dados: list) -> None:
+        pass
+
+    def entra(self, dados: list) -> None:
+        pass
+
+    def lideres(self) -> None:
+        pass
+
+    def l(self) -> None:
+        pass
+
+    def inicia(self) -> None:
+        pass
+
+    def desafio(self, dados: list) -> None:
+        pass
+
+    def move(self, dados: list) -> None:
+        direcao = dados
+        return f'{MOVE}{len(direcao)}{direcao}'
+
+    def atraso(self) -> None:
+        return f'{ATRASO}'    
+
+    def encerra(self) -> None:
+        return f'{ENCERRA}'
+
+    def sai(self, dados: list) -> None:
+        pass
+
+    def tchau(self) -> None:
+        pass
+
+    def executa_comando(self, dados: list):
+        pass
+
+class Cliente:
     _estado = None
 
     def __init__(self, estado: Estados) -> None:
@@ -206,6 +223,46 @@ class Clientes:
     def clientes_executa_comando(self, dados: list) -> None:
         self._estado.executa_comando(dados)
 
+    #pacman deve ter um método que transcreve o tabuleiro para uma string
+    #pacman deve ser instanciado ao receber um tabuleiro em lista
+    #talvez o init do pacman nao deva ser o jogo propriamente dito
+    #se eu fizer um método que de fato inicia, eu conseguiria modularizar o codigo
+    #para obter o comportamento de que um cliente tem uma instancia de pacman
+    #mas o jogo só começa quando ele faz uma chamada do metodo start_game. 
+    # Esse start game que receberia o tabuleiro em lista.
+    #essa separação seria apenas para que eu pudesse utilizar os métodos 
+    #serializa_tabuleiro e deserializa_tabuleiro...
+    def envia_tabuleiro(self, dados: list):  
+        #return f'{TABULEIRO}{linhas}{colunas}{tabuleiro_to_str}'
+        pass
+
+    def recebe_tabuleiro(self, dados: list):
+        pass
+
+class ClienteTCP(Cliente):
+    _estado = None
+    def __init__(self, host, port) -> None:
+        super().__init__(EstadosNeutro())
+        self._host = host
+        self._port = port
+        self._usuario = 'u'
+        self._senha = 's'
+        self._latencia = []
+        self._heartbeat = []
+        print(self._estado, self._host, self._port, self._usuario, self._senha, self._latencia, self._heartbeat)
+    
+
+class ClienteUDP(Cliente):
+    pass
 
 if __name__ == '__main__':
-    print('Olá, mundo!')
+    dummy, host, port, tipo = sys.argv
+    c = ''
+    if tipo == 'TCP':
+        c = ClienteTCP(host, port)
+    elif tipo == 'UDP':
+        c = ClienteUDP(host, port)
+    else:
+        c = ClienteTCP(host, port)
+
+    
